8ff1ccd5d0e4c40907f164663c832e52fe4b9e4d
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-05-07 17:40:31 +0200

    added changes

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function menuOnClick() {
     const nav = document.getElementById("nav");
     nav.classList.toggle("active");
 }
+
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+    alert("Hello, world!");
+});
