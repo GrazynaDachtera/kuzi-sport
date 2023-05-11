174c0605bd7b948efe7c88213cb34ce9561997b0
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-05-11 19:07:04 +0100

    added changes

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ const myButton = document.getElementById("myButton");
 myButton.addEventListener("click", function() {
     alert("Hello, world!");
 });
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+
+    // Do something with the email and message data, such as sending it to a server
+    console.log(`Email: ${email}, Message: ${message}`);
+
+    form.reset();
+});
+
