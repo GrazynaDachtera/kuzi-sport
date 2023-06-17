1ae6711c8970225860f5bdab1380fc35bc1258aa
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-06-17 19:15:30 +0100

    changed menu in js

--- timetable/timetable.js
+++ timetable/timetable.js
@@ -3,5 +3,20 @@ function toggleMenu() {
     nav.classList.toggle("active");
 }
 
-<div id='idancesoft_schedule'></div>
-<script>make_module('schedule', 1000017051, '1564', '', 'undefined', 'undefined', 'undefined', 'idancesoft_schedule') ;</script>
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "about/about.html";
+});
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+
+    console.log(`Email: ${email}, Message: ${message}`);
+
+    form.reset();
+});
+
