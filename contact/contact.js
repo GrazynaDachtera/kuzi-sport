e48b3c5a16ba65ec7d4375f1ad1200b95ef4f32c
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-09-13 15:48:12 +0200

    added changes

--- contact/contact.js
+++ contact/contact.js
@@ -7,16 +7,33 @@ document.getElementById("myButton").addEventListener("click", function() {
     window.location.href = "about/about.html";
 });
 
-const form = document.getElementById('contact-form');
+<script src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
+<script>
+    (function(){
+    emailjs.init("O7cFRHfbQC40hpcOT");
+})();
+</script>
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+<script>
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    const email = form.elements.email.value;
-    const message = form.elements.message.value;
+    // your public key
+    var publicKey = 'O7cFRHfbQC40hpcOT';
 
-    console.log(`Email: ${email}, Message: ${message}`);
+    // your service ID
+    var serviceID = 'service_gb3ea08';
 
-    form.reset();
+    // your template ID
+    var templateID = 'template_iyma3le';
+
+    emailjs.sendForm(serviceID, templateID, this, publicKey)
+    .then(function() {
+    console.log('Email successfully sent!');
+})
+    .catch(function(error) {
+    console.error('Error occurred:', error);
 });
+});
+</script>
 
